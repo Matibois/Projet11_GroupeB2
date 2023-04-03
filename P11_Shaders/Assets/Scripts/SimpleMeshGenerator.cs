96d2d9937c46cda2e3611a0dbde0dae5288f9ea7
Matibois <100830614+Matibois@users.noreply.github.com>
2023-04-03 15:19:55 +0200

    first exercice

--- P11_Shaders/Assets/Scripts/SimpleMeshGenerator.cs
+++ P11_Shaders/Assets/Scripts/SimpleMeshGenerator.cs
@@ -6,18 +6,80 @@ public class SimpleMeshGenerator : MonoBehaviour
 {
     public Material _MeshMaterial;
 
+    [SerializeField] float x = 0;
+    [SerializeField] float y = 0;
+
     void Start()
     {
-        //MakeTriangle();
-        
+        MakeTriangle();
+        //MakeDoubleQuad();
+
     }
 
     void MakeTriangle()
     {
-		// TO DO: Vertices array of type Vector3
-		// TO DO: Indices array of type int
-        
-		// TO DO: appeller la fonction BuildMesh avec les bons paramètres
+        Vector3[] vertices = new Vector3[]
+        {
+            new Vector3(0,0,0),
+            new Vector3(0,1,0),
+            new Vector3(1,0,0),
+        };
+
+        int[] indices = new int[]
+        {
+            0, 1, 2,
+        };
+
+        BuildMesh("Triangle", vertices, indices);
+    }
+
+    void MakeQuad()
+    {
+
+        Vector3[] vertices = new Vector3[]
+        {
+            new Vector3(0,0,0),
+            new Vector3(0,1,0),
+            new Vector3(1,1,0),
+            new Vector3(1,0,0),
+        };
+
+        int[] indices = new int[]
+        {
+            0, 1, 2,
+            0, 2, 3
+        };
+
+        BuildMesh("Quad", vertices, indices);
+    }
+
+    void MakeDoubleQuad()
+    {
+
+        Vector3[] vertices = new Vector3[]
+        {
+            new Vector3(0,0,0),
+            new Vector3(0,1,0),
+            new Vector3(1,1,0),
+            new Vector3(1,0,0),
+
+            new Vector3(2,0,0),
+            new Vector3(2,1,0),
+            new Vector3(3,1,0),
+            new Vector3(3,0,0),
+        };
+
+        int[] indices = new int[]
+        {
+            0, 1, 2,
+            0, 2, 3,
+
+            4, 5, 6,
+            4, 6, 7
+        };
+
+        BuildMesh("DoubleQuad", vertices, indices);
+
     }
 
     protected void BuildMesh(string gameObjectName, Vector3[] vertices, int[] indices, Vector2[] uvs = null)
@@ -29,11 +91,11 @@ public class SimpleMeshGenerator : MonoBehaviour
 
         // Create a GameObject
         GameObject primitive = new GameObject(gameObjectName);
-        
+
         // Add the components...
         MeshRenderer meshRenderer = primitive.AddComponent<MeshRenderer>();
         MeshFilter meshFilter = primitive.AddComponent<MeshFilter>();
-      
+
         // ... and set the mesh buffers. 
         meshFilter.mesh.vertices = vertices;
         meshFilter.mesh.triangles = indices;
