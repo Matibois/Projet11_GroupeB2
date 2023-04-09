56767dfca66210cf98a40d0c1ee362b563a33c57
YRGTech <yrocher@gaming.tech>
2023-04-09 06:23:42 +0200

    tests a droite a gauche

--- P11_Shaders/Assets/Scripts/TopDownShooterController.cs
+++ P11_Shaders/Assets/Scripts/TopDownShooterController.cs
@@ -4,16 +4,16 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.InputSystem;
+
 public class TopDownShooterController : MonoBehaviour
 {
     [SerializeField] private LayerMask _aimColliderMask;
-    //[SerializeField] private Transform _debugTransform;
     [SerializeField] private Transform _pfBulletProjectile;
     [SerializeField] private Transform _spawnBulletPosition;
     private ThirdPersonController _thirdPersonController;
     private StarterAssetsInputs _starterAssetsInputs;
     private Animator _animator;
-    // Start is called before the first frame update
+
     void Start()
     {
         _thirdPersonController = GetComponent<ThirdPersonController>();
@@ -22,43 +22,28 @@ public class TopDownShooterController : MonoBehaviour
         _animator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         Vector3 mouseWorldPosition = Vector3.zero;
-        Ray ray = Camera.main.ScreenPointToRay(Mouse.current.position.ReadValue());
+
+        // Get the mouse position in screen space
+        Vector3 mouseScreenPosition = Mouse.current.position.ReadValue();
+        Ray ray = Camera.main.ScreenPointToRay(mouseScreenPosition);
         Transform hitTransform = null;
+
         if (Physics.Raycast(ray, out RaycastHit hitInfo, 999f, _aimColliderMask))
         {
-            //_debugTransform.position = hitInfo.point;
             mouseWorldPosition = hitInfo.point;
             hitTransform = hitInfo.transform;
         }
 
-
-        Vector3 worldAimTarget = mouseWorldPosition;
-        worldAimTarget.y = transform.position.y;
-        Vector3 aimDirection = (worldAimTarget - transform.position).normalized;
+        // Set the aim direction towards the mouse world position
+        Vector3 aimDirection = (mouseWorldPosition - transform.position).normalized;
         transform.forward = Vector3.Lerp(transform.forward, aimDirection, Time.deltaTime * 10);
-
 
         if (_starterAssetsInputs.shoot)
         {
-            //hitscan methode
-            //if (hitTransform != null)
-            //{ // hit something
-            //    if (hitTransform.GetComponent<BulletTarget>() != null)
-            //    {
-            //        //hit target 
-            //        Instantiate(_pfBulletProjectile, _spawnBulletPosition.position, Quaternion.LookRotation(aimDirection, Vector3.up));
-            //    }
-            //    else
-            //    {
-            //        //hit something else
-            //    }
-            //}
-            //projectile methode
-            Instantiate(_pfBulletProjectile, _spawnBulletPosition.position, Quaternion.LookRotation(aimDirection,Vector3.up));
+            Instantiate(_pfBulletProjectile, _spawnBulletPosition.position, Quaternion.LookRotation(aimDirection, Vector3.up));
             _starterAssetsInputs.shoot = false;
         }
     }
