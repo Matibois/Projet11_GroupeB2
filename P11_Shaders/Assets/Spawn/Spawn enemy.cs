9b96cae23d36e8a0c350b93f489f6f00671aa5c4
Matibois <100830614+Matibois@users.noreply.github.com>
2023-04-13 18:03:57 +0200

    rober's spawn

--- P11_Shaders/Assets/Spawn/Spawn enemy.cs
+++ P11_Shaders/Assets/Spawn/Spawn enemy.cs
@@ -5,14 +5,12 @@ using UnityEngine;
 
 public class Spawnenemy : MonoBehaviour
 {
-    [SerializeField] GameObject enemy;
+    [SerializeField] private GameObject enemy;
     float temps = 5f;
     public Vector3 spawnPos;
     // Start is called before the first frame update
     void Start()
     {
-            enemy.transform.position = spawnPos;
-        
     }
 
     // Update is called once per frame
@@ -20,9 +18,9 @@ public class Spawnenemy : MonoBehaviour
     {
         if (Time.time > temps)
         {
+            enemy.transform.position = spawnPos;
             temps += 5f;
-            Instantiate(enemy);
-            
+            Instantiate(enemy);   
         }
     }
 }
