67f743bb653302b0ef5a475a007d6dcc99369539
Matibois <100830614+Matibois@users.noreply.github.com>
2023-04-14 00:08:45 +0200

    spawn

--- P11_Shaders/Assets/Atrier/Spawn/Spawn enemy.cs
+++ P11_Shaders/Assets/Atrier/Spawn/Spawn enemy.cs
@@ -5,19 +5,14 @@ using UnityEngine;
 
 public class Spawnenemy : MonoBehaviour
 {
-    [SerializeField] GameObject enemy;
+    [SerializeField] private GameObject enemy;
     float temps = 5f;
     public Vector3 spawnPos;
-    // Start is called before the first frame update
-    void Start()
-    {
-            enemy.transform.position = spawnPos;
-        
-    }
 
-    // Update is called once per frame
     void Update()
     {
+        enemy.transform.position = spawnPos;
+        
         if (Time.time > temps)
         {
             temps += 5f;
